8f3494352e9f7f2c50bbaa0b889e8ce53a64c688
mattn <mattn.jp@gmail.com>
2013-01-21 20:15:30 +0900

    remove ().

--- example/example.rb
+++ example/example.rb
@@ -17,30 +17,30 @@ end
 db.execute_batch('delete from foo')
 db.execute_batch('insert into foo(text) values(?)', 'foo')
 db.execute_batch('insert into foo(text) values(?)', 'bar')
-db.transaction()
+db.transaction
 db.execute_batch('insert into foo(text) values(?)', 'baz')
-db.rollback()
-db.transaction()
+db.rollback
+db.transaction
 db.execute_batch('insert into foo(text) values(?)', 'bazoooo!')
-db.commit()
+db.commit
 
-db.transaction()
+db.transaction
 (1..100).each_with_index {|x,i|
   db.execute_batch('insert into bar(id, text) values(?, ?)', i, x)
 }
-db.commit()
+db.commit
 
 db.execute('select * from foo') do |row, fields|
   puts row
 end
 
-puts db.execute('select id from foo where text = ?', 'foo').next()[0]
+puts db.execute('select id from foo where text = ?', 'foo').next[0]
 
 row = db.execute('select * from bar')
-puts row.fields()
-while cols = row.next()
+puts row.fields
+while cols = row.next
   puts cols
 end
-row.close()
+row.close
 
-db.close()
+db.close
